bc29e029d85b7ad605497f5c6fae267b2121d15b
Elise <robotbrainify@gmail.com>
2019-03-19 10:47:26 -0400

    globals

--- main.c
+++ main.c
@@ -7,23 +7,23 @@
 /* Includes ****************************************************************/
 /***************************************************************************/
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<errno.h>
-#include<math.h>
+#include <errno.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include<clcg4.h>
+#include <clcg4.h>
 
-#include<mpi.h>
-#include<pthread.h>
+#include <mpi.h>
+#include <pthread.h>
 
 // #define BGQ 1 // when running BG/Q, comment out when testing on mastiff
 
 #ifdef BGQ
-#include<hwi/include/bqc/A2_inlines.h>
+#include <hwi/include/bqc/A2_inlines.h>
 #else
-#define GetTimeBase MPI_Wtime            
+#define GetTimeBase MPI_Wtime
 #endif
 
 /***************************************************************************/
@@ -31,7 +31,12 @@
 /***************************************************************************/
 
 #define ALIVE 1
-#define DEAD  0
+#define DEAD 0
+
+#define CHECK(cell) (cell & 1)
+#define BIRTH(cell) (cell |= 2)
+#define KILL(cell) (cell &= ~2)
+#define COMMIT(cell) (cell >>= 1)
 
 /***************************************************************************/
 /* Global Vars *************************************************************/
@@ -39,11 +44,13 @@
 
 double g_time_in_secs = 0;
 double g_processor_frequency = 1600000000.0; // processing speed for BG/Q
-unsigned long long g_start_cycles=0;
-unsigned long long g_end_cycles=0;
+unsigned long long g_start_cycles = 0;
+unsigned long long g_end_cycles = 0;
 
 // You define these
 
+typedef unsigned char row[rowlen];
+row* chunk;
 
 /***************************************************************************/
 /* Function Decs ***********************************************************/
@@ -51,37 +58,35 @@ unsigned long long g_end_cycles=0;
 
 // You define these
 
-
 /***************************************************************************/
 /* Function: Main **********************************************************/
 /***************************************************************************/
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-//    int i = 0;
+    //    int i = 0;
     int mpi_myrank;
     int mpi_commsize;
-// Example MPI startup and using CLCG4 RNG
-    MPI_Init( &argc, &argv);
-    MPI_Comm_size( MPI_COMM_WORLD, &mpi_commsize);
-    MPI_Comm_rank( MPI_COMM_WORLD, &mpi_myrank);
-    
-// Init 32,768 RNG streams - each rank has an independent stream
-    InitDefault();
-    
-// Note, used the mpi_myrank to select which RNG stream to use.
-// You must replace mpi_myrank with the right row being used.
-// This just show you how to call the RNG.    
-    printf("Rank %d of %d has been started and a first Random Value of %lf\n", 
-	   mpi_myrank, mpi_commsize, GenVal(mpi_myrank));
-    
-    MPI_Barrier( MPI_COMM_WORLD );
-    
-// Insert your code
-    
+    // Example MPI startup and using CLCG4 RNG
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &mpi_commsize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_myrank);
 
-// END -Perform a barrier and then leave MPI
-    MPI_Barrier( MPI_COMM_WORLD );
+    // Init 32,768 RNG streams - each rank has an independent stream
+    InitDefault();
+
+    // Note, used the mpi_myrank to select which RNG stream to use.
+    // You must replace mpi_myrank with the right row being used.
+    // This just show you how to call the RNG.
+    printf("Rank %d of %d has been started and a first Random Value of %lf\n",
+        mpi_myrank, mpi_commsize, GenVal(mpi_myrank));
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Insert your code
+
+    // END -Perform a barrier and then leave MPI
+    MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();
     return 0;
 }
